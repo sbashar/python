39f5dbae95a8304c4030a5c137111cbcaae9db68
Saydul Bashar <tamimnoone@gmail.com>
2019-05-06 19:59:48 +0100

    Fixed typo

--- src/hackerrank/challenges/py_if_else.py
+++ src/hackerrank/challenges/py_if_else.py
@@ -10,13 +10,13 @@ def is_wierd(number):
     If n is even and in the inclusive range of 6 to 20, print Weird
     If n is even and greater than 20, print Not Weird
     Returns:
-        [str]: [returns Wierd if number is Wierd otherwise returns Not Wierd]
+        [str]: [returns Weird if number is Weird otherwise returns Not Weird]
     """
 
-    result = "Wierd"
+    result = "Weird"
 
     if number % 2 == 0:
         if 2 <= number <= 5 or number > 20:
-            return "Not Wierd"
+            return "Not Weird"
 
     return result
--- tests/hackerrank/challenges/test_py_if_else.py
+++ tests/hackerrank/challenges/test_py_if_else.py
@@ -5,24 +5,24 @@ from src.hackerrank.challenges import py_if_else
 
 def test_is_wierd():
     """Tests for is_wierd"""
-    # if n is odd, print Wierd
-    assert py_if_else.is_wierd(0) == "Wierd"
-    assert py_if_else.is_wierd(1) == "Wierd"
-    assert py_if_else.is_wierd(3) == "Wierd"
-    assert py_if_else.is_wierd(5) == "Wierd"
-    assert py_if_else.is_wierd(7) == "Wierd"
-    assert py_if_else.is_wierd(19) == "Wierd"
-    assert py_if_else.is_wierd(21) == "Wierd"
+    # if n is odd, print Weird
+    assert py_if_else.is_wierd(0) == "Weird"
+    assert py_if_else.is_wierd(1) == "Weird"
+    assert py_if_else.is_wierd(3) == "Weird"
+    assert py_if_else.is_wierd(5) == "Weird"
+    assert py_if_else.is_wierd(7) == "Weird"
+    assert py_if_else.is_wierd(19) == "Weird"
+    assert py_if_else.is_wierd(21) == "Weird"
 
     # If n is even and in the inclusive range of 2 to 5, print Not Weird
-    assert py_if_else.is_wierd(2) == "Not Wierd"
-    assert py_if_else.is_wierd(4) == "Not Wierd"
+    assert py_if_else.is_wierd(2) == "Not Weird"
+    assert py_if_else.is_wierd(4) == "Not Weird"
 
     # If n is even and in the inclusive range of 6 to 20, print Weird
-    assert py_if_else.is_wierd(6) == "Wierd"
-    assert py_if_else.is_wierd(8) == "Wierd"
-    assert py_if_else.is_wierd(18) == "Wierd"
-    assert py_if_else.is_wierd(20) == "Wierd"
+    assert py_if_else.is_wierd(6) == "Weird"
+    assert py_if_else.is_wierd(8) == "Weird"
+    assert py_if_else.is_wierd(18) == "Weird"
+    assert py_if_else.is_wierd(20) == "Weird"
 
     # If n is even and greater than 20, print Not Weird
-    assert py_if_else.is_wierd(22) == "Not Wierd"
+    assert py_if_else.is_wierd(22) == "Not Weird"
